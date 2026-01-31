7c12f9ec26913236a1b08974079b6045a4bc6f89
kevargaso <kevargaso@unal.edu.co>
2026-01-30 22:06:24 -0500

    Add all consolidaciones data to ConsolidadoFull page

--- src/components/admin/ConsolidadoFull.jsx
+++ src/components/admin/ConsolidadoFull.jsx
@@ -48,11 +48,335 @@ export default function ConsolidadoFull() {
                     order: "386900949",
                     tienda: "NewEgg",
                     cliente: "Kevin",
-                    producto: "Combo (RAM + Board)",
+                    producto: "Combo (RAM + Board ASUS ROG STRIX B850-A GAMING WIFI R2 Blanca)",
                     cantidad: 2,
                     precioUnitarioUSD: 189.99,
                     pesoUnitario: 5.5,
                     tracking: "1ZR43Y850330991594"
+                }
+            ]
+        },
+        {
+            id: "LBX3150708G",
+            totalPeso: 22,
+            costoEnvioTotalCOP: 449633,
+            items: [
+                {
+                    guiaInterna: "LBX3149997",
+                    order: "578809931",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Board ASUS TUF GAMING B850M-E WIFI",
+                    cantidad: 2,
+                    precioUnitarioUSD: 169.99,
+                    pesoUnitario: 4,
+                    tracking: "1ZR43Y850331191223"
+                },
+                {
+                    guiaInterna: "LBX3150338",
+                    order: "113-0331687-1745014",
+                    tienda: "Amazon",
+                    cliente: "Laura",
+                    producto: "iPad A16 11 Gen Color Rosa",
+                    cantidad: 3,
+                    precioUnitarioUSD: 279.99,
+                    pesoUnitario: 2,
+                    tracking: "1ZX341F40300845645"
+                },
+                {
+                    guiaInterna: "LBX3149796",
+                    order: "405473679",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Board ASUS TUF GAMING B850M-E WIFI",
+                    cantidad: 2,
+                    precioUnitarioUSD: 169.99,
+                    pesoUnitario: 4,
+                    tracking: "1ZE569860306925120"
+                }
+            ]
+        },
+        {
+            id: "LBX3150706G",
+            totalPeso: 24,
+            costoEnvioTotalCOP: 487102,
+            items: [
+                {
+                    guiaInterna: "LBX3149349",
+                    order: "405456099",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Board ASUS TUF GAMING B850M-E WIFI",
+                    cantidad: 2,
+                    precioUnitarioUSD: 169.99,
+                    pesoUnitario: 4,
+                    tracking: "1ZE569860306858962"
+                },
+                {
+                    guiaInterna: "LBX3149361",
+                    order: "566957692",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Board ASUS B850 MAX GAMING WIFI W",
+                    cantidad: 2,
+                    precioUnitarioUSD: 159.99,
+                    pesoUnitario: 4,
+                    tracking: "1ZE569860306891121"
+                },
+                {
+                    guiaInterna: "LBX3149356",
+                    order: "405452439",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Board ASUS B850 MAX GAMING WIFI W",
+                    cantidad: 2,
+                    precioUnitarioUSD: 159.99,
+                    pesoUnitario: 4,
+                    tracking: "1ZE569860306852753"
+                }
+            ]
+        },
+        {
+            id: "LBX3151388G",
+            totalPeso: 36,
+            costoEnvioTotalCOP: 721445,
+            items: [
+                {
+                    guiaInterna: "LBX3151040",
+                    order: "578950131",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Combo (16Gb RAM DDR4 y BOARD B550)",
+                    cantidad: 5,
+                    precioUnitarioUSD: 99.99,
+                    pesoUnitario: 3.2,
+                    tracking: "1ZR43Y850331396557"
+                },
+                {
+                    guiaInterna: "LBX3151074",
+                    order: "567165392",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Combo (RAM y BOARD ASUS ROG STRIX B850-A GAMING WIFI R2)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 189.99,
+                    pesoUnitario: 5.5,
+                    tracking: "1ZR43Y850331244621"
+                },
+                {
+                    guiaInterna: "LBX3151072",
+                    order: "567250732",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Combo (DDR5 32GB RAM, BOARD ASRock B850I Lightning WiFi y COOLER)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 369.99,
+                    pesoUnitario: 4.5,
+                    tracking: "1ZR43Y850331307143"
+                }
+            ]
+        },
+        {
+            id: "LBX3151389G",
+            totalPeso: 36,
+            costoEnvioTotalCOP: 721445,
+            items: [
+                {
+                    guiaInterna: "LBX3151056",
+                    order: "558346294",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Board B550",
+                    cantidad: 5,
+                    precioUnitarioUSD: 99.99,
+                    pesoUnitario: 3,
+                    tracking: "1ZR43Y850331347654"
+                },
+                {
+                    guiaInterna: "LBX3151080",
+                    order: "578950131",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Combo (DDR5 32GB RAM, BOARD ASRock B850I Lightning WiFi y COOLER)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 369.99,
+                    pesoUnitario: 4.5,
+                    tracking: "1ZR43Y850331397350"
+                },
+                {
+                    guiaInterna: "LBX3151099",
+                    order: "558179014",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Combo (RAM y BOARD ASUS ROG STRIX B850-A GAMING WIFI R2)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 189.99,
+                    pesoUnitario: 5.5,
+                    tracking: "1ZR43Y850331232956"
+                },
+                {
+                    guiaInterna: "LBX3151346",
+                    order: "578809931",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Kit 16Gb RAM DDR5 (Parte de Combo LBX3149997)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 0,
+                    pesoUnitario: 0.5,
+                    tracking: "1ZE569860307111015"
+                }
+            ]
+        },
+        {
+            id: "LBX3151846G",
+            totalPeso: 19,
+            costoEnvioTotalCOP: 398693,
+            items: [
+                {
+                    guiaInterna: "LBX3151528",
+                    order: "558182914",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Combo (CPU Ryzen 9600x BOARD RAM COOLER )",
+                    cantidad: 1,
+                    precioUnitarioUSD: 319.99,
+                    pesoUnitario: 9.5,
+                    tracking: "1ZE569860307176867"
+                },
+                {
+                    guiaInterna: "LBX3151595",
+                    order: "578859431",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Combo (CPU Ryzen 9600x BOARD RAM COOLER )",
+                    cantidad: 1,
+                    precioUnitarioUSD: 319.99,
+                    pesoUnitario: 9.5,
+                    tracking: "1ZE569860307182298"
+                }
+            ]
+        },
+        {
+            id: "LBX3150704G",
+            totalPeso: 18,
+            costoEnvioTotalCOP: 374694,
+            items: [
+                {
+                    guiaInterna: "LBX3148914",
+                    order: "-",
+                    tienda: "-",
+                    cliente: "-",
+                    producto: "Meta Quest 3 512GB",
+                    cantidad: 1,
+                    precioUnitarioUSD: 407.10,
+                    pesoUnitario: 3,
+                    tracking: "420331669339589677058529028454"
+                },
+                {
+                    guiaInterna: "LBX3149081",
+                    order: "405456099",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Kit 16Gb RAM DDR5 (Parte de combo con LBX3149349)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 0,
+                    pesoUnitario: 0.5,
+                    tracking: "1ZR43Y850331015057"
+                },
+                {
+                    guiaInterna: "LBX3149083",
+                    order: "566957692",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Kit 16Gb RAM DDR5 (Parte de combo con LBX3149361)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 0,
+                    pesoUnitario: 0.5,
+                    tracking: "1ZR43Y850331016770"
+                },
+                {
+                    guiaInterna: "LBX3149173",
+                    order: "405473679",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Combo (RAM y BOARD ASUS ROG STRIX B850-A GAMING WIFI R2)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 189.99,
+                    pesoUnitario: 5.5,
+                    tracking: "1ZR43Y850331050929"
+                },
+                {
+                    guiaInterna: "LBX3149173",
+                    order: "405473679",
+                    tienda: "NewEgg",
+                    cliente: "Natalia",
+                    producto: "Kit 16Gb RAM DDR5 (Parte de combo con LBX3149796)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 0,
+                    pesoUnitario: 0.5,
+                    tracking: "1ZR43Y850331050929"
+                },
+                {
+                    guiaInterna: "LBX3149399",
+                    order: "405452439",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Kit 16Gb RAM DDR5 (Parte de combo con LBX3149356)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 0,
+                    pesoUnitario: 0.5,
+                    tracking: "1ZR43Y850331019759"
+                }
+            ]
+        },
+        {
+            id: "LBX3151885G",
+            totalPeso: 16,
+            costoEnvioTotalCOP: 341737,
+            items: [
+                {
+                    guiaInterna: "LBX3151599",
+                    order: "113-9559246-9111420",
+                    tienda: "Amazon",
+                    cliente: "Laura",
+                    producto: "Ipad AIR M3 128GB COLOR GRIS",
+                    cantidad: 1,
+                    precioUnitarioUSD: 400.00,
+                    pesoUnitario: 2,
+                    tracking: "1ZX427651359428974"
+                },
+                {
+                    guiaInterna: "LBX3151748",
+                    order: "113-9053265-3036207 / 113-9559246-9111420",
+                    tienda: "Amazon",
+                    cliente: "Laura",
+                    producto: "Ipad AIR M3 128GB COLOR GRIS (Envío Cruzado)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 400.00,
+                    pesoUnitario: 2,
+                    tracking: "1ZG1937J1315356161"
+                },
+                {
+                    guiaInterna: "LBX3151604",
+                    order: "558346294",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Combo (32Gb RAM DDR4 y BOARD B550)",
+                    cantidad: 2,
+                    precioUnitarioUSD: 209.99,
+                    pesoUnitario: 3.75,
+                    tracking: "1ZE569860307328103"
+                },
+                {
+                    guiaInterna: "LBX3151604",
+                    order: "558346294",
+                    tienda: "NewEgg",
+                    cliente: "Kevin",
+                    producto: "Kit 16Gb RAM DDR5 (Parte de combo con LBX3151056)",
+                    cantidad: 5,
+                    precioUnitarioUSD: 0,
+                    pesoUnitario: 0.5,
+                    tracking: "1ZE569860307328103"
                 }
             ]
         }
